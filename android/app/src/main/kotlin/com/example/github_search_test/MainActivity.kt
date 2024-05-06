5a12bf477c5f1b2063017a2717fd37bcdeaf2a12
Artem Luzin m <aratar006@gmail.com>
2024-05-06 20:26:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/github_search_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.github_search_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:github_search_test/src/features/app.dart';
+
+void main() {
+  runApp(const App());
+}
