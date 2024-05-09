36b313babc76f21c12c6d03687845bc3a3260a2f
Artem Luzin m <aratar006@gmail.com>
2024-05-09 23:18:25 +0700

    Сделать минимальную вёрстку страницы поиска

--- lib/src/features/app.dart
+++ lib/src/features/app.dart
@@ -4,14 +4,12 @@ import 'package:github_search_test/src/features/search/search_screen.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
       ),
       home: const SearchScreen(),
     );
--- lib/src/features/search/search_screen.dart
+++ lib/src/features/search/search_screen.dart
@@ -7,8 +7,53 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    final size = MediaQuery.of(context).size;
+    final height = size.height;
+    final width = size.width;
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+          body: CustomScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        slivers: [
+          const SliverAppBar(
+            title: Text('GitHub юзерs'),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(40))),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 16,
+                  ),
+                  label: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.search),
+                      Text('Поиск'),
+                    ],
+                  ),
+                ),
+                onSubmitted: (value) {
+                  //TODO: implement search logic
+                },
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([]),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: width * 2,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
