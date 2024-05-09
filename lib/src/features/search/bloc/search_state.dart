8331ba3c77f2da110e09d075b6b9a9f0ca90954b
Artem Luzin m <aratar006@gmail.com>
2024-05-09 23:33:22 +0700

    Добавить основу для блока

---
+++ lib/src/features/search/bloc/search_state.dart
@@ -0,0 +1,9 @@
+part of 'search_bloc.dart';
+
+@freezed
+class SearchState with _$SearchState {
+  const factory SearchState({
+    @Default(ScreenStatus.initial) ScreenStatus status,
+    @Default([]) List searchElements,
+  }) = _SearchState;
+}
