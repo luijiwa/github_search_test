c3950a56422bf7ad6a362e3861c8b3d39cbac91e
Artem Luzin m <aratar006@gmail.com>
2024-05-14 21:29:13 +0700

    Поменять место импорта

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:github_search_test/main.dart';
-import 'package:github_search_test/src/features/app.dart';
+import 'package:github_search_test/src/features/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
