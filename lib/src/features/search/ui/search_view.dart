db790dd4129c1c81d0b11a75012343d5944f7d2a
Artem Luzin m <aratar006@gmail.com>
2024-05-14 21:29:03 +0700

    Перенести виджет в отдельный файл

--- lib/src/features/search/ui/search_view.dart
+++ lib/src/features/search/ui/search_view.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:github_search_test/src/core/models/screen_status.dart';
 import 'package:github_search_test/src/features/search/bloc/search_bloc.dart';
+import 'package:github_search_test/src/features/search/ui/users_list_widget.dart';
 
 class SearchView extends StatefulWidget {
   const SearchView({
@@ -80,78 +80,5 @@ class _SearchViewState extends State<SearchView> {
         )
       ],
     ));
-  }
-}
-
-class UsersListWidget extends StatelessWidget {
-  const UsersListWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final width = MediaQuery.sizeOf(context).width;
-
-    return SliverPadding(
-      padding: EdgeInsets.symmetric(horizontal: width * 0.05),
-      sliver: BlocBuilder<SearchBloc, SearchState>(
-        buildWhen: (previous, current) => previous.items != current.items,
-        builder: (context, state) {
-          return SliverList.separated(
-            itemCount: state.items.length,
-            itemBuilder: (context, index) {
-              return Stack(children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage:
-                          NetworkImage(state.items[index].avatarUrl),
-                    ),
-                    const SizedBox(width: 8),
-                    Text(state.items[index].login),
-                    const Spacer(),
-                    BlocBuilder<SearchBloc, SearchState>(
-                      buildWhen: (previous, current) =>
-                          previous.items[index].followersCount !=
-                          current.items[index].followersCount,
-                      builder: (context, state) {
-                        final followersCount =
-                            state.items[index].followersCount;
-                        if (followersCount == null) {
-                          return const SizedBox(
-                            width: 10,
-                            height: 10,
-                            child: CircularProgressIndicator(
-                              strokeWidth: 2,
-                            ),
-                          );
-                        } else {
-                          return Text(
-                              state.items[index].followersCount.toString());
-                        }
-                      },
-                    ),
-                    const Icon(Icons.chevron_right),
-                  ],
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  height: 60,
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: () {
-                        log(state.items[index].login);
-                      },
-                    ),
-                  ),
-                )
-              ]);
-            },
-            separatorBuilder: (__, _) => const SizedBox(height: 10),
-          );
-        },
-      ),
-    );
   }
 }
